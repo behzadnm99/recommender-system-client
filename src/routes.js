9cf744cf03894fdcf228fca2b3acd404522a5324
behzad <behzadnm99@gmail.com>
2019-04-29 16:45:47 +0430

    add new routes

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,17 @@
-export const SINGUP = '/users/';
-export const LOGIN = '/users/login';
-export const CURRENT_USER = '/users/current';   
+export const USER_SINGUP = '/users/';
+export const USER_LOGIN = '/users/login/';
+export const USER_CURRENT = '/users/current/';
+export const USER_REMOVE = '/users/';
+export const USER_EDIT = '/users/';
+ 
+export const BOOKS_GET_ALL = '/books/get/all/';
+export const BOOKS_GET_BY_ID = '/books/get/';
+export const BOOKS_CREATE = '/books/add/';
+export const BOOKS_REMOVE = '/books/';
+export const BOOKS_EDIT = '/books/';
+
+export const MOVIES_GET_ALL = '/movies/get/all';
+export const MOVIES_GET_BY_ID = '/movies/get/';
+export const MOVIES_CREATE = '/movies/add';
+export const MOVIES_REMOVE = '/movies/';
+export const MOVIES_EDIT = '/movies/';
